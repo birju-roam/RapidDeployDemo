b437e3c389bd654f82e0ca4f2153eaa49b66d67c
birju-roam <birju@roam.ai>
2024-08-30 16:11:24 +0530

    updated roam and mqtt builds

--- app/src/main/java/com/example/myapplication/MainApplication.kt
+++ app/src/main/java/com/example/myapplication/MainApplication.kt
@@ -10,7 +10,8 @@ class MainApplication:Application() {
         super.onCreate()
 
         //roam core
-        Roam.initialize(this, "db4de26a9d8edc115a779d722dcc033d42018924024bea34ace18fc541236ff2")
+        //Roam.initialize(this, "db4de26a9d8edc115a779d722dcc033d42018924024bea34ace18fc541236ff2")//expired
+        Roam.initialize(this, "66126a7c68aebe84c5f2b1a6e1a6f45fc5f5928729554a02437e703d2086e358")
 
         //roam mqtt connector
         RoamMQTTConnector.initialize(this)
